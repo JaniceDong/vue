4137f8ac881594f77bc2d6f4092a381e66e55f72
dzz <1581996825@qq.com>
2017-06-06 09:57:47 +0800

    更换vue目录

---
+++ vue/sell/src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">
+    <site-header :seller="seller"></site-header>
+    <ul class="tab border-1px border-2px">
+      <li>
+        <router-link to="/goods">商品</router-link>
+      </li>
+      <li>
+        <router-link to="/ratings">评论</router-link>
+      </li>
+      <li>
+        <router-link to="/seller">商家</router-link>
+      </li>
+    </ul>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import SiteHeader from './components/header/header.vue';
+
+  const ERR_OK = 0;
+
+  export default {
+    data() {
+      return {
+        seller: {
+        }
+      };
+    },
+    components: {
+      SiteHeader
+    },
+    created() {
+      this.$http.get('/api/seller').then((response) => {
+        response = response.body;
+        if (response.errno === ERR_OK) {
+          this.seller = response.data;
+        }
+      });
+    }
+  };
+</script>
+
+<style rel="stylesheet/style" type="text/css">
+  ul {
+    display: flex;
+    height: 4rem;
+    justify-content: space-around;
+    align-items: center;
+  }
+
+  li {
+    flex: 1;
+    text-align: center;
+    list-style: none;
+    height:100%;
+    line-height:4rem;;
+  }
+
+  a {
+    color: #42b983;
+    display: block;
+    font-size: 20px;
+    color:rgb(77,85,93);
+  }
+  a.active{
+    color:rgb(240,20,20);
+  }
+
+</style>
